e9d25e224a7b57232b672aa060a04c1e13c0e137
BAHUANNN <1421363448@qq.com>
2018-07-28 14:57:56 +0800

    A

--- Dragon/FilmDragon/app/src/main/java/com/muxistudio/filmgragon/utils/AlertDialogUtils.java
+++
@@ -1,8 +0,0 @@
-package com.muxistudio.filmgragon.utils;
-
-/**
- * Created by kolibreath on 18-3-31.
- */
-
-public class AlertDialogUtils {
-}
--- StepDemo/app/src/main/java/com/bignerdranch/android/stepdemo/base/StepCountListener.java
+++
@@ -1,9 +0,0 @@
-package com.bignerdranch.android.stepdemo.base;
-
-/**
- * Created by zdf on 18-4-22.
- */
-
-interface StepCountListener {
-    void countStep();
-}
